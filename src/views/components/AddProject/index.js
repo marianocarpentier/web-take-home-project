b89e27b7288aaf3acb894d1d838243f64fd1181f
Mariano Carpentier <mariano.carpentier@gmail.com>
2017-12-19 01:06:04 +1100

    Added project and contract values

--- src/views/components/AddProject/index.js
+++ src/views/components/AddProject/index.js
@@ -1,26 +1,57 @@
 import React, {Component} from 'react';
 import {connect} from 'react-redux';
 import {Button, FormControl, InputLabel, Input, Select, MenuItem} from 'material-ui';
+import {PROJECT_TYPES, CONTRACT_VALUES} from 'util/Constants';
 import './AddProject.css';
 
 class AddProject extends Component {
-    state = {};
-    load = this.load.bind(this);
 
-    async load(...args) {
-        // try {
-        //     this.setState({ loading: true, error: false });
-        //     const data = await loadData(...args);
-        //     this.setState({ loading: false, data });
-        // } catch (ex) {
-        //     this.setState({ loading: false, error: true });
-        // }
+    constructor(props) {
+        super(props);
+        this.state = {
+            projectTypeItems: [],
+            contractValueItems: [],
+            selectedProjectId: "",
+            selectedContractValueId: "",
+        }
+        this.selectProject = this.selectProject.bind(this);
+        this.selectContract = this.selectContract.bind(this);
+    }
+
+    selectProject(event) {
+        this.setState(...this.state, {
+            selectedProjectId: event.target.value
+        })
+    }
+
+    selectContract(event) {
+        this.setState(...this.state, {
+            selectedContractValueId: event.target.value
+        })
+    }
+
+    componentWillMount() {
+
+        this.setState(...this.state, {
+            projectTypeItems: PROJECT_TYPES.map(prj =>
+                <MenuItem key={prj.id} value={prj.id}>
+                    {prj.name}
+                </MenuItem>),
+            contractValueItems: CONTRACT_VALUES.map(val =>
+                <MenuItem key={val.id} value={val.id}>
+                    {val.description}
+                </MenuItem>)
+        });
     }
 
     render() {
+
+        const {projectTypeItems, contractValueItems, selectedProjectId, selectedContractValueId} = this.state;
+        const {selectProject, selectContract} = this;
+
         return (
             <div className="add-projects-cont">
-                <h2>Add a project you've worked on</h2>
+                <h2>Add a project you&apos;ve worked on</h2>
                 <div className="upload-cont">
                     <Button className="button secondary-button">Upload photos</Button>
                 </div>
@@ -28,11 +59,10 @@ class AddProject extends Component {
                     <FormControl className="form-control" fullWidth>
                         <InputLabel htmlFor="type">Select project type</InputLabel>
                         <Select
-                            value=""
-                            input={<Input name="type" id="type"/>}>
-                            <MenuItem value="">
-                                <em>None</em>
-                            </MenuItem>
+                            value={selectedProjectId}
+                            onChange={selectProject}
+                            input={<Input name="type" id="type" />}>
+                            {projectTypeItems}
                         </Select>
                     </FormControl>
                     <FormControl className="form-control" fullWidth>
@@ -45,14 +75,10 @@ class AddProject extends Component {
                     <FormControl className="form-control" fullWidth>
                         <InputLabel htmlFor="value">Select a contract value</InputLabel>
                         <Select
-                            value=""
+                            value={selectedContractValueId}
+                            onChange={selectContract}
                             input={<Input name="value" id="value"/>}>
-                            <MenuItem value="">
-                                <em>None</em>
-                            </MenuItem>
-                            <MenuItem value={10}>$10</MenuItem>
-                            <MenuItem value={20}>$20</MenuItem>
-                            <MenuItem value={30}>$30</MenuItem>
+                            {contractValueItems}
                         </Select>
                     </FormControl>
                     <FormControl className="form-control" fullWidth>
